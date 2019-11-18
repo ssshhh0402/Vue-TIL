8c857ff64a0e9c26d087fffb95c200156e9dca44
ssshhh0402 <ssshhh0402@naver.com>
2019-11-18 13:55:25 +0900

    Django | Init Todo App

--- todo-django/todo_django/settings.py
+++ todo-django/todo_django/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
+    'todos',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'todos.User'
